6d669515067450cb42ae2b031b0cf500388ce99f
unknown <nazar.bylen.ir.2021@lpnu.ua>
2023-11-17 19:39:39 +0200

    internet gateway + rout tables

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -26,3 +26,33 @@ resource "aws_subnet" "subnet3" {
   map_public_ip_on_launch = true
   availability_zone       = "eu-central-1c"
 }
+
+resource "aws_internet_gateway" "internet-gateway" {
+  vpc_id = aws_vpc.vpc-main.id
+  tags = {
+    Name = "internet-gateway"
+  }
+}
+
+resource "aws_route_table" "route-table" {
+  vpc_id = aws_vpc.vpc-main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-gateway.id
+  }
+}
+
+resource "aws_route_table_association" "subnet-route" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route-table.id
+}
+
+resource "aws_route_table_association" "subnet2-route" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.route-table.id
+}
+
+resource "aws_route_table_association" "subnet3-route" {
+  subnet_id      = aws_subnet.subnet3.id
+  route_table_id = aws_route_table.route-table.id
+}
