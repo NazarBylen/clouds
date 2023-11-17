da01e13ecf286307dda488af4abc1bcac09d35c9
unknown <nazar.bylen.ir.2021@lpnu.ua>
2023-11-17 19:40:28 +0200

    security groups

---
+++ infrastructure/security_groups.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "security-group" {
+  name   = "ecs-security-group"
+  vpc_id = aws_vpc.vpc-main.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "80"
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "443"
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 3000
+    to_port     = 3000
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "3000"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  depends_on = [aws_vpc.vpc-main]
+}
