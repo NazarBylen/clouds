d7ebb5382ef723b374b82a42c11933dd3e8e656d
unknown <nazar.bylen.ir.2021@lpnu.ua>
2023-11-19 17:09:38 +0200

    ecs service

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -57,3 +57,28 @@ resource "aws_ecs_task_definition" "ecs-task-definition" {
 resource "aws_ecs_cluster" "ecs-cluster" {
   name = "ecs-cluster"
 }
+
+resource "aws_ecs_service" "esc-service" {
+  name            = "nodejs-app-service"
+  cluster         = aws_ecs_cluster.ecs-cluster.id
+  task_definition = aws_ecs_task_definition.ecs-task-definition.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups  = [aws_security_group.security-group.id]
+    subnets          = [aws_subnet.subnet.id, aws_subnet.subnet2.id, aws_subnet.subnet3.id]
+    assign_public_ip = true
+  }
+
+  force_new_deployment = true
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.ecs-tg.id
+    container_name   = "node-js-app"
+    container_port   = 3000
+  }
+
+  depends_on = [aws_lb_listener.ecs_alb_listener, aws_ecs_task_definition.ecs-task-definition]
+}
+
